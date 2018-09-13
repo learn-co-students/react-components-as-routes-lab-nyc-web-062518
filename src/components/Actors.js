dd1b40604ec3b1c7513c0e4ef3e788555468bdd7
cmaniscalchi <claudia.maniscalchi@gmail.com>
2018-09-12 20:32:24 -0400

    Done.

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -1,12 +1,26 @@
-import React from 'react';
-import { actors } from '../data';
+import React from 'react'
+import { actors } from '../data'
 
 const Actors = () => {
+
+  let actorsDivs = actors.map(actor => {
+    return (
+      <div>
+        {actor.name}
+        <ul>
+          {actor.movies.map(movie => <li>{movie}</li>)}
+        </ul>
+      </div>
+    )
+  })
+
+
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Actors Page</h1>
+      {actorsDivs}
     </div>
-  );
-};
+  )
+}
 
-export default Actors;
+export default Actors
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,12 +1,25 @@
-import React from 'react';
-import { movies } from '../data';
+import React from 'react'
+import { movies } from '../data'
 
 const Movies = () => {
+
+  let moviesDivs = movies.map(movie => {
+    return (
+      <div>
+        {movie.title} ({movie.time} Minutes)
+        <ul>
+          {movie.genres.map(genre => <li>{genre}</li>)}
+        </ul>
+      </div>
+    )
+  })
+
   return (
     <div>
-        {/*{code here}*/}
+      <h1>Movies Page</h1>
+        {moviesDivs}
     </div>
-  );
-};
+  )
+}
 
 export default Movies;
